3225b2f888cfe51468b58224f3dbc408448e6168
Igor Malyugin <i.malyugin00@gmail.com>
2024-10-25 12:32:54 +0300

    Buttons

--- src/5_entities/post/ui/PostActions.vue
+++ src/5_entities/post/ui/PostActions.vue
@@ -1,9 +1,19 @@
 <template>
 
   <div class="actions">
-      <button class="btn btn-like">Like</button>
-      <button class="btn btn-trash">Trash</button>
-      <button class="actions__comments">Open comments</button>
+      <Button className="like">
+        <Heart />
+        <span class="btn-text"> Like </span>
+        <span class="btn-count">192</span>
+      </Button>
+      <Button className="dislike">
+        <Dislike />
+        <span class="btn-text"> Trash </span>
+        <span class="btn-count">192</span>
+      </Button>
+
+      <button class="comments-btn">Open comments</button>
+      <div class="comments-last">Today</div>
 
       <div class="tags">
         <div class="tags__item">history</div>
@@ -16,9 +26,13 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import Button from '@/6_shared/ui/Button.vue';
+import Heart from '@/6_shared/icons/Heart.vue';
+import Dislike from '@/6_shared/icons/Dislike.vue';
 
 export default defineComponent({
   name: 'PostActions',
+  components: { Button, Heart, Dislike },
 });
 </script>
 
@@ -33,6 +47,47 @@ export default defineComponent({
   .tags {
     display: flex;
     align-items: center;
+  }
+
+  .like {
+    border-radius: 30px 0px 0px 30px;
+    margin-right: 1px;
+  }
+  .dislike {
+    border-radius: 0px 30px 30px 0px;
+    margin-right: 8px;
+  }
+
+  .btn-count {
+    color: #040405;
+  }
+
+  .btn-text {
+    margin: 0px 6px 0px 4px;
+  }
+
+  .comments-btn {
+    background: none;
+    color: var(--primary);
+    position: relative;
+    overflow: visible;
+    margin-right: 8px;
+  }
+  .comments-btn::after {
+      z-index: 99;
+      content: '';
+      position: absolute;
+      width: 100%;
+      height: 1px;
+      background: var(--primary);
+      opacity: 0.5;
+      top: 100%;
+      left: 0;
+  }
+
+  .comments-last {
+    font: var(--font-caption);
+    color: rgba(4, 4, 5, 0.22)
   }
 
   .tags__item {
---
+++ src/6_shared/ui/Button.vue
@@ -0,0 +1,34 @@
+<template>
+
+  <button :class="[className, 'btn']"> <slot></slot> </button>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Button',
+  props: {
+    className: {
+      type: String,
+      default: '',
+    },
+  }
+});
+
+</script>
+
+<style>
+
+  .btn {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: var(--gray);
+    height: 27px;
+    width: 95px;
+    cursor: pointer;
+  }
+
+</style>
